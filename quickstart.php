868810c5c6daabab047cd632479c9838fd7d021b
Sithum <sithum.dashantha@gmail.com>
2022-08-22 16:25:58 +0530

    new XML data set imported

--- quickstart.php
+++ quickstart.php
@@ -1,7 +1,6 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
+require_once "./error_report.php";
 
 require 'vendor/autoload.php'; // include Composer's autoloader
 
